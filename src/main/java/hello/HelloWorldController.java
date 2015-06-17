34995d2b80b0057eb7eb62686aa2b18a175bc77c
Yogesh Bhardwaj <ybhardwaj@manh.com>
2015-06-17 10:53:42 -0400

    Created first version of Spring Boot Hello World application, packaged as an executable jar

---
+++ src/main/java/hello/HelloWorldController.java
@@ -0,0 +1,22 @@
+package hello;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloWorldController {
+    
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello, World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldController.class, args);
+    }
+}
+
